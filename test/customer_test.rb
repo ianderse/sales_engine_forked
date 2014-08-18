b01041796588b9f509e8ecf8cfd892a79ced74d6
Ian Andersen <ianderse@mac.com>
2014-08-18 11:32:13 -0600

    update customer_test

--- test/customer_test.rb
+++ test/customer_test.rb
@@ -4,30 +4,36 @@ require_relative '../lib/csv_handler'
 require_relative '../lib/customer'
 
 class CustomerTest < Minitest::Test
-  attr_reader :customers
+  attr_reader :customers, :customer
 
   def setup
     csv        = CsvHandler.new("./data/customers.csv")
     @customers = csv.data.collect {|row| Customer.new(row)}
+    @customer = customers.first
   end
 
   def test_it_returns_a_first_name
-    assert_equal "Joey", customers.first.first_name
+    assert_equal "Joey", customer.first_name
   end
 
   def test_it_returns_a_last_name
-    assert_equal "Ondricka", customers.first.last_name
+    assert_equal "Ondricka", customer.last_name
   end
 
   def test_it_returns_an_id
-    assert_equal "1", customers.first.id
+    assert_equal "1", customer.id
   end
 
   def test_it_returns_an_updated_at
-    assert_equal "2012-03-27 14:54:09 UTC", customers.first.created_at
+    assert_equal "2012-03-27 14:54:09 UTC", customer.created_at
   end
 
   def test_it_returns_created_at
-    assert_equal "2012-03-27 14:54:09 UTC", customers.first.created_at
+    assert_equal "2012-03-27 14:54:09 UTC", customer.created_at
+  end
+
+  def test_it_returns_all_associated_invoices
+    skip
+    assert_equal ??, customer .invoices
   end
 end
