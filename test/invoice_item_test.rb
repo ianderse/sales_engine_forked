31d6374cb8623d183116ca74d7fa5bed6b77a26e
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-08-19 10:44:24 -0600

    added invoice_item_tests

--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -2,13 +2,17 @@ require 'csv'
 require_relative 'test_helper'
 require_relative '../lib/invoice_item'
 require_relative '../lib/csv_handler'
+require_relative '../lib/sales_engine'
 
 class InvoiceItemTest < Minitest::Test
   attr_reader :sample
 
   def setup
+    @engine = SalesEngine.new
+    @engine.startup
+
     csv = CsvHandler.new("./data/invoice_items.csv")
-    @sample = csv.data.collect {|row| InvoiceItem.new(row)}
+    @sample = csv.data.collect {|row| InvoiceItem.new(row, @engine.invoice_item_repository)}
   end
 
   def test_it_returns_an_id
@@ -33,6 +37,10 @@ class InvoiceItemTest < Minitest::Test
 
   def test_it_returns_invoice_id
     assert_equal "1", sample.first.invoice_id
+  end
+
+  def test_it_knows_its_own_items
+    assert_equal "26",  sample.first.item.merchant_id
   end
 
 end
