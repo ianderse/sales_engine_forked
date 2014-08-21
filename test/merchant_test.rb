63db347295e949c54981e3cc67aa7d8a60d30125
Ian Andersen <ianderse@mac.com>
2014-08-21 09:24:02 -0600

    update tests for merchant

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -63,6 +63,7 @@ class MerchantTest < Minitest::Test
 		transaction = Minitest::Mock.new
 		invoice.expect :transactions, [ transaction ]
 		transaction.expect :result, "success"
+		transaction.expect :successful_transaction?, true
 		invoice.expect :invoice_items, [ invoice_item, invoice_item_2 ]
 		invoice_item.expect :item_revenue, "5"
 		invoice_item_2.expect :item_revenue, "30"
